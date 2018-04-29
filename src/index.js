180a4e09d32f1fdadba3db733449a389ef6119d6
Beata Karolczak <karolczak.beata@gmail.com>
2018-04-29 12:39:45 +0200

    Implement table

--- src/index.js
+++ src/index.js
@@ -4,10 +4,44 @@ import employees from './data/data.json';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
+const EmployeeRow = ({id, firstName, lastName, dateOfBirth, company, note}) => (
+	<tr>
+		<td>{id}</td>
+		<td>{firstName}</td>
+		<td>{lastName}</td>
+		<td>{dateOfBirth}</td>    
+		<td>{company}</td>
+		<td>{note}</td>      
+	</tr>
+);
+
 class EmployeeTable extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			employees: employees,
+		};
+	}
+
 	render() {
+		
+		const rows = this.state.employees.map((rowData) => <EmployeeRow key={rowData.id} {...rowData} />);
+
 		return (
-			<div> Employees App
+			<div>
+				<table className="table table-striped">
+	        		<thead className="table-info">
+	        			<th>Id</th>
+	        			<th>First Name</th>
+	        			<th>Last Name</th>
+	        			<th>Date of Birth</th>
+	        			<th>Company</th>
+	        			<th>Note</th>
+	        		</thead>
+			        <tbody>
+			          {rows}
+			        </tbody>
+	      		</table>
 			</div>
 		);		
 	}
