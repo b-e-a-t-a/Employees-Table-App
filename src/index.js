21c7c39c44d5fba5d282ce5284fa362787d99555
Beata Karolczak <karolczak.beata@gmail.com>
2018-04-29 13:25:30 +0200

    Add sorting

--- src/index.js
+++ src/index.js
@@ -23,15 +23,15 @@ class SearchBar extends React.Component {
 	render() {
 		return (
 			<div>
-        		<input 
-        			type="text"
-        			placeholder="Search..."
-        			value={this.props.filterText}
-        			ref="filterTextInput"
-        			onChange={this.handleChange.bind(this)}
-        		/>
+				<input 
+					type="text"
+					placeholder="Search..."
+					value={this.props.filterText}
+					ref="filterTextInput"
+					onChange={this.handleChange.bind(this)}
+				/>
 			</div>
-    	);
+		);
 	}
 }
 
@@ -41,14 +41,39 @@ class EmployeeTable extends React.Component {
 		this.state = {
 			employees: employees,
 			filterText: '',
+			isSortedAsc: false,
 		};
+		this.compareBy.bind(this);
+		this.sortBy.bind(this);
 	}
 
 	handleUserInput(filterText) {
-    	this.setState({
-    		filterText: filterText,
-    	});
-  	};
+		this.setState({
+			filterText: filterText,
+		});
+	}
+	compareBy(key) {
+		return function (a, b) {
+			if (a[key] < b[key]) return -1;
+			if (a[key] > b[key]) return 1;
+			return 0;
+		};
+	}
+	uncompareBy(key) {
+		return function (a, b) {
+			if (a[key] > b[key]) return -1;
+			if (a[key] < b[key]) return 1;
+			return 0;
+		};
+	}
+	sortBy(key) {
+		let newEmployees = [...this.state.employees];
+		this.state.isSortedAsc ? newEmployees.sort(this.uncompareBy(key)) : newEmployees.sort(this.compareBy(key));
+		this.setState({
+			employees: newEmployees,
+			isSortedAsc: !this.state.isSortedAsc
+		});
+	}
 
 	render() {
 		const filterText = this.state.filterText;
@@ -63,33 +88,33 @@ class EmployeeTable extends React.Component {
 		return (
 			<div>
 				<table className="table table-striped">
-	        		<thead className="table-info">
-	        			<th>Id</th>
-	        			<th>First Name</th>
-	        			<th>Last Name</th>
-	        			<th>Date of Birth</th>
-	        			<th>Company</th>
-	        			<th>Note</th>
-	        		</thead>
-			        <tbody>
-			        	<tr>
-			        		<td>
-			        			<SearchBar filterText={this.state.filterText} onUserInput={this.handleUserInput.bind(this)}/>
-			        		</td>
-			        		<td>
-			        		</td>
-			        		<td>
-			        		</td>
-			        		<td>
-			        		</td>
-			        		<td>
-			        		</td>
-			        		<td>
-			        		</td>
-			        	</tr>
+					<thead className="table-info">
+						<th onClick={() => this.sortBy('id')} >Id {' \u2193'}{' \u2191'}</th>
+						<th onClick={() => this.sortBy('firstName')}>First Name {' \u2193'}{' \u2191'}</th>
+						<th onClick={() => this.sortBy('lastName')}>Last Name {' \u2193'}{' \u2191'}</th>
+						<th onClick={() => this.sortBy('dateOfBirth')}>Date of Birth {' \u2193'}{' \u2191'}</th>
+						<th onClick={() => this.sortBy('company')}>Company {' \u2193'}{' \u2191'}</th>
+						<th onClick={() => this.sortBy('note')}>Note {' \u2193'}{' \u2191'}</th>
+					</thead>
+					<tbody>
+						<tr>
+							<td>
+								<SearchBar filterText={this.state.filterText} onUserInput={this.handleUserInput.bind(this)}/>
+							</td>
+							<td>
+							</td>
+							<td>
+							</td>
+							<td>
+							</td>
+							<td>
+							</td>
+							<td>
+							</td>
+						</tr>
 						{rows}
-			        </tbody>
-	      		</table>
+					</tbody>
+				</table>
 			</div>
 		);		
 	}
